4a50de3f5d1d81c1a5676ec2bf534102ca83f015
nammmm <krissdavjn@gmail.com>
2016-05-29 15:21:02 -0400

    created aboutus page

---
+++ wp-content/themes/the-next/aboutus.php
@@ -0,0 +1,178 @@
+<?php
+/*
+Template Name: AboutUs
+*/
+
+if (!defined('ABSPATH')) exit;
+
+get_header();
+
+?>
+<head>
+	<link rel="stylesheet" href="<?php echo THENEXT_THEME_URL . '/css/animate.min.css'; ?>" />
+</head>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-12">
+					<div>
+						<h3 class="text-center wow zoomIn animated" style="font-size: 16pt; font-family: Helvetica, Tahoma, Arial, STXihei, Heiti, Microsoft YaHei, sans-serif; display: block; text-align: center; margin: 10px 20%; line-height: 1.5; visibility: visible; animation: zoomIn; -webkit-animation: zoomIn;">我们很厉害，真的。</h3>
+					</div>
+					<hr class="separator-style5" style="margin: 30px 0">
+				</div>
+				<div class="row" style="padding:40px 0px 30px 0px;background-repeat:no-repeat;background-attachment:scroll;background-position:left top;">
+					<div class="col-md-4">
+						<h3 class="strong" >
+							美国留学申请很厉害
+						</h3>
+						<p style="font-size: 15px;">
+							各种常春藤
+						</p>
+					</div>
+					<div class="col-md-4">
+						<h3 class="strong">
+							加拿大留学申请很厉害
+						</h3>
+						<p style="font-size: 15px;">
+							各种Top10
+						</p>
+					</div>
+					<div class="col-md-4">
+						<h3 class="strong">
+							总之就是很厉害
+						</h3>
+						<p style="font-size: 15px;">
+							各种厉害
+						</p>
+					</div>
+				</div>
+				<!-- 成员介绍 -->
+				<div class="row" style="padding:25px 0px 0px 0px;">
+					<div>
+						<h3 class="text-center wow zoomIn animated" style="font-size: 16pt; font-family: Helvetica, Tahoma, Arial, STXihei, Heiti, Microsoft YaHei, sans-serif; display: block; text-align: center; margin: 10px 20%; line-height: 1.5; visibility: visible; animation: zoomIn; -webkit-animation: zoomIn;">文书团队</h3>
+					</div>
+					<hr class="separator" style="padding:30px 0px 0px 0px;">
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+					   	<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/kls.jpg'; ?>">
+        					<h3>柯林斯(Collins)</h3>
+        					<p class="nationality">国籍：美国</p>        
+        					<p class="undergrad">本科：密西根大学</p>
+        					<p class="grad">硕士：密西根大学</p>
+        					<p class="major">专业：人类学；分子生物学；健康管理及政策研究</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/xfr.png'; ?>">
+        					<h3>谢珐瑞（Shafli）</h3>
+        					<p class="nationality">国籍：美国</p>        
+        					<p class="undergrad">本科：哥伦比亚大学</p>
+        					<p class="grad">硕士：耶鲁大学</p>
+        					<p class="major">专业：生物学</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/ptk.jpg'; ?>">
+        					<h3>帕托克（Patrick）</h3>
+        					<p class="nationality">国籍：英国</p>        
+        					<p class="undergrad">本科：剑桥大学</p>
+        					<p class="grad">硕士：剑桥大学</p>
+        					<p class="major">专业：文学</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/alf.jpg'; ?>">
+        					<h3>奥利弗（Olive）</h3>
+        					<p class="nationality">国籍：英国</p>        
+        					<p class="undergrad">本科：纽卡斯尔大学</p>
+        					<p class="major">专业：跨文化交流</p>
+    					</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+					   	<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/al.jpg'; ?>">
+        					<h3>艾琳（Aillen）</h3>
+        					<p class="nationality">国籍：美国</p>        
+        					<p class="undergrad">本科：密西根大学</p>
+        					<p class="grad">硕士：耶鲁大学</p>
+        					<p class="major">专业：大脑，行为和认知科学；人类学；公共医疗</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/jm.jpg'; ?>">
+        					<h3>嘉蜜（Jamie）</h3>
+        					<p class="nationality">国籍：英国</p>        
+        					<p class="undergrad">本科：纽卡斯尔大学</p>
+        					<p class="major">专业：跨文化交流</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/lcd1.jpg'; ?>">
+        					<h3>理查德（Richard）</h3>
+        					<p class="nationality">国籍：美国</p>        
+        					<p class="undergrad">本科：密西根大学</p>
+        					<p class="phd">博士：波士顿大学</p>
+        					<p class="major">专业：生物学；法律学</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+					</div>
+				</div>
+				<div class="row" style="padding:25px 0px 0px 0px;">
+					<div>
+						<h3 class="text-center wow zoomIn animated" style="font-size: 16pt; font-family: Helvetica, Tahoma, Arial, STXihei, Heiti, Microsoft YaHei, sans-serif; display: block; text-align: center; margin: 10px 20%; line-height: 1.5; visibility: visible; animation: zoomIn; -webkit-animation: zoomIn;">文化活动和讲座老师</h3>
+					</div>
+					<hr class="separator" style="padding:30px 0px 0px 0px;">
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+					   	<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/lcd2.jpg'; ?>">
+        					<h3>Michal. B</h3>
+        					<p>男</p>
+        					<p class="nationality">出生地：美国</p>        
+        					<p class="undergrad">本科：Occidental College</p>
+        					<p class="major">专业：商业管理</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/lcd2.jpg'; ?>">
+        					<h3>Nadav</h3>
+        					<p>男</p>
+        					<p class="nationality">出生地：以色列</p>        
+        					<p class="undergrad">本科：海法大学 (TAU)</p>
+        					<p class="major">专业：医学</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thumbnail" style="border: 1px solid #DDDDDD;">
+    						<img src="<?php echo THENEXT_THEME_URL . '/images/team-members/lcd2.jpg'; ?>">
+        					<h3>天露（Tal）</h3>
+        					<p>女</p>
+        					<p class="nationality">出生地：特拉维夫</p>        
+        					<p class="undergrad">本科：Stony Brook University-SUNY </p>
+        					<p class="major">专业：社会学/护理</p>
+    					</div>
+					</div>
+					<div class="col-md-3">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+<?php get_footer(); 
